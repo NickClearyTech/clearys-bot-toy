c6f3f7323db551fa785db307ebb7ae0fd06029c6
Nick Cleary <nicholascleary01@gmail.com>
2022-10-09 13:53:45 -0400

    fixing user ID

--- app/cbt/message_handlers/memes.py
+++ app/cbt/message_handlers/memes.py
@@ -5,7 +5,7 @@ import logging
 
 
 # A bot handler to reply with "THE PROPHET HAS SPOKEN" whenever richard mentions emacs in the emacs channel
-@message_handler(channel=992166504269357146, user=992166504269357146)
+@message_handler(channel=992166504269357146, user=192024972644974592)
 async def prophet_has_spoken(message: Message):
     if "emacs" in message.content.lower():
         await message.reply("*THE PROPHET HAS SPOKEN*")
