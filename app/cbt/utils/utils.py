267cabbe9917b9ee2d13340ceaacd3aab354429f
Richard Sent <richard@freakingpenguin.com>
2023-12-20 17:55:45 -0500

    Emacs concat strings

--- app/cbt/utils/utils.py
+++ app/cbt/utils/utils.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_token():
-    if os.environ.get("DISCORD_TOKEN", None) is None:
+    if os.environ.get(emacs_concat_string("DISCORD_", "TOKEN"), None) is None:
         print("ERROR: INVALID TOKEN")
         exit(1)
     return os.environ.get("DISCORD_TOKEN", None)
