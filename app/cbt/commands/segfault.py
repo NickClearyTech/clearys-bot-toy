f1b8081992e48a6e5118c3757d67304b23616c87
Richard Sent <richard@freakingpenguin.com>
2024-01-05 14:59:35 -0500

    Format

--- app/cbt/commands/segfault.py
+++ app/cbt/commands/segfault.py
@@ -5,12 +5,12 @@ from discord import app_commands
 from utils.client import tree
 from utils.utils import get_server, logger
 
+
 @tree.command(name="segfault", guild=discord.Object(get_server()))
-async def segfault(
-    interaction: discord.Interaction
-):
+async def segfault(interaction: discord.Interaction):
     await interaction.response.send_message("BRB KMS")
     logger.info("Segfault triggered")
-    import ctypes;ctypes.string_at(0)
-    logger.info("We failed to segfault. Cry about it!")
+    import ctypes
 
+    ctypes.string_at(0)
+    logger.info("We failed to segfault. Cry about it!")
