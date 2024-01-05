4e6edaf4b5fa817fad184828d0dcbfdb66c72805
Nick Cleary <nicholascleary01@gmail.com>
2024-01-05 17:07:29 -0500

    no

--- app/cbt/commands/segfault.py
+++ app/cbt/commands/segfault.py
@@ -3,11 +3,14 @@ import discord
 from discord import app_commands
 
 from utils.client import tree
-from utils.utils import get_server, logger
+from utils.utils import get_server, logger, get_chance
 
 
 @tree.command(name="segfault", guild=discord.Object(get_server()))
 async def segfault(interaction: discord.Interaction):
+    if get_chance():
+        await interaction.response.send_message("No u")
+        return
     await interaction.response.send_message("BRB KMS")
     logger.info("Segfault triggered")
     import ctypes
